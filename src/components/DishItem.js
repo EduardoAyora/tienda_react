7ed1db88939c9ae8bd0860198c75575d65547b9c
Eduardo Ayora <eduardoaayora64@gmail.com>
2020-09-04 20:40:08 -0500

    avances en iconos para seleccionar productos

--- src/components/DishItem.js
+++ src/components/DishItem.js
@@ -3,55 +3,121 @@ import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faArrowLeft, faArrowRight } from '@fortawesome/free-solid-svg-icons';
 
-export function DishItem({dish}) {
-    const pricesArray = dish.precios;
-    if(pricesArray.length === 1) {
-        return (
-            <li className="dish-item">
-                <div className="dish-item-image-container">
-                    <img src={dish.imagen} alt={dish.nombre} />
-                </div>
-                <div className="dish-item-info">
-                    <h4 className="dish-item-name">{dish.nombre}</h4>
-                    <p>{dish.price}</p>
-                </div>
-            </li>
-        )
+export class DishItem extends React.Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            quantity: 1
+        }
+
+        this.buttonMinusClick = this.buttonMinusClick.bind(this);
+        this.buttonPlusClick = this.buttonPlusClick.bind(this);
+        this.inputChange = this.inputChange.bind(this);
     }
-    else if(pricesArray.length > 1) {
-        return (
-            <li className="dish-item">
 
-                <button type="button" className="dish-item-arrow left-item-arrow" onClick={() => console.log('s')}>
-                    <FontAwesomeIcon icon={faArrowLeft} className="fa-lg" />
-                </button>
-
-                <div className="dish-item-image-container">
-                    <img src={dish.imagen} alt={dish.nombre} />
-                </div>
-                <div className="dish-item-info">
-                    <h4 className="dish-item-name">{dish.nombre}</h4>
-                    <p>{dish.price}</p>
-                </div>
-
-                <button type="button" className="dish-item-arrow right-item-arrow" onClick={() => console.log('s')}>
-                    <FontAwesomeIcon icon={faArrowRight} className="fa-lg" />
-                </button>
-
-            </li>
-        )
+    buttonMinusClick() {
+        let quantity = this.state.quantity;
+        if(quantity > 1) {
+            this.setState({
+                quantity: quantity - 1
+            })
+        }
     }
-    else {
-        return (
-            <li className="dish-item">
-                <div className="dish-item-image-container">
-                    <img src={dish.imagen} alt={dish.nombre} />
-                </div>
-                <div className="dish-item-info">
-                    <h4 className="dish-item-name">{dish.nombre}</h4>
-                </div>
-            </li>
-        )
+
+    buttonPlusClick() {
+        let quantity = this.state.quantity;
+        if(quantity !== '') {
+            this.setState({
+                quantity: quantity + 1
+            })
+        } else {
+            this.setState({
+                quantity: 1
+            })
+        }
     }
+
+    inputChange(event) {
+        let value = event.target.value
+        if (value !== '') {
+            if(value > 0) {
+                this.setState({
+                    quantity: parseInt(value)
+                })
+            } else {
+                this.setState({
+                    quantity: 1
+                })
+            }
+        }
+        else {
+            this.setState({
+                quantity: value
+            })
+        }
+    }
+
+    render() {
+        let dish = this.props.dish;
+        let quantity = this.state.quantity;
+        const pricesArray = dish.precios;
+
+        if(pricesArray.length === 1) {
+            return (
+                <DishItemContainer dish={dish}>
+                    <span>{pricesArray[0].nombre}</span>
+                    <div>
+                        {pricesArray[0].precio}
+                    </div>
+                    <div className="dish-item-input-group">
+                        <button className="dish-item-button-minus" onClick={this.buttonMinusClick}>-</button>
+                        <input type="number" value={quantity} className="dish-item-quantity-field"
+                            onChange={this.inputChange} />
+                        <button className="dish-item-button-plus" onClick={this.buttonPlusClick}>+</button>
+                    </div>
+                </DishItemContainer>
+            )
+        }
+        else if(pricesArray.length > 1) {
+            return (
+                <DishItemContainer dish={dish}>
+                    <button type="button" className="dish-item-arrow left-item-arrow" onClick={() => console.log('s')}>
+                        <FontAwesomeIcon icon={faArrowLeft} className="fa-lg" />
+                    </button>
     
+                    <span>{pricesArray[0].nombre}</span>
+                    <div>
+                        {pricesArray[0].precio}
+                    </div>
+    
+                    <button type="button" className="dish-item-arrow right-item-arrow" onClick={() => console.log('s')}>
+                        <FontAwesomeIcon icon={faArrowRight} className="fa-lg" />
+                    </button>
+                </DishItemContainer>
+            )
+        }
+        else {
+            return (
+                <DishItemContainer dish={dish}></DishItemContainer>
+            )
+        }
+    }
+
+}
+
+function DishItemContainer(props) {
+    const dish = props.dish;
+    return (
+        <li className="dish-item">
+            <div className="dish-item-image-container">
+                <img src={dish.imagen} alt={dish.nombre} />
+            </div>
+            <div className="dish-item-info">
+                <h4 className="dish-item-name">{dish.nombre}</h4>
+                {props.children}
+            </div>
+        </li>
+    )
 }
--- src/json/data.js
+++ src/json/data.js
@@ -14,19 +14,16 @@ export const data = [
                         id: 1,
                         precio: 0.7,
                         nombre: 'pequeña',
-                        id_producto: 1,
                     },
                     {
                         id: 1,
                         precio: 2,
                         nombre: 'mediana',
-                        id_producto: 1,
                     },
                     {
                         id: 1,
                         precio: 3.25,
                         nombre: 'grande',
-                        id_producto: 1,
                     },
                 ]
             },
@@ -34,7 +31,13 @@ export const data = [
                 id: 2,
                 nombre: 'Gaseosa',
                 imagen: '/images/productos/gaseosa.jpg',
-                precios: []
+                precios: [
+                    {
+                        id: 4,
+                        precio: 0.7,
+                        nombre: 'unidad',
+                    },
+                ]
             },
             {
                 id: 3,
